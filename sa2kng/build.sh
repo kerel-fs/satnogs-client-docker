e42c24879676d51561795cf5185861ce958d137c
Fabian P. Schmidt <kerel@mailbox.org>
2025-03-11 15:22:26 +0100

    build.sh: Change branches

--- sa2kng/build.sh
+++ sa2kng/build.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 export DOCKER_BUILDKIT=1
-TAG="sa2kng"
-REPO_ROOT="knegge"
-GNURADIO_IMAGE_TAG="3.8.2.0-satnogs"
 
-ARGS="  --build-arg GNURADIO_IMAGE_TAG=${GNURADIO_IMAGE_TAG}"
-ARGS+=" --build-arg GRSATNOGS_URL=https://gitlab.com/knegge/gr-satnogs.git"
-ARGS+=" --build-arg GRSATNOGS_BRANCH=${TAG}"
-ARGS+=" --build-arg GRSATNOGS_VER=2.3.5.0+2+${TAG}"
-ARGS+=" --build-arg GRSOAPY_URL=https://gitlab.com/knegge/gr-soapy.git"
-ARGS+=" --build-arg GRSOAPY_BRANCH=${TAG}"
-ARGS+=" --build-arg GRSOAPY_VER=2.1.3.1+2+${TAG}"
-ARGS+=" --build-arg FLOWGRAPHS_URL=https://gitlab.com/knegge/satnogs-flowgraphs.git"
-ARGS+=" --build-arg FLOWGRAPHS_BRANCH=${TAG}"
-ARGS+=" --build-arg FLOWGRAPHS_VER=1.5+2+${TAG}"
-ARGS+=" --build-arg CLIENT_URL=https://gitlab.com/knegge/satnogs-client.git"
-ARGS+=" --build-arg CLIENT_BRANCH=${TAG}"
-#ARGS+=" --build-arg RTLSDR_URL=https://github.com/osmocom/rtl-sdr.git"
-#ARGS+=" --build-arg RTLSDR_BRANCH=master"
-#ARGS+=" --build-arg RTLSDR_VER=0.6.0+2+${TAG}"
-#ARGS+=" --build-arg UHD_URL=http://archive.ubuntu.com/ubuntu/pool/universe/u/uhd/uhd_4.1.0.5-3.dsc"
-#ARGS+=" --build-arg OBS_REPO=https://download.opensuse.org/repositories/home:/knegge:/branches:/home:/librespace:/satnogs-unstable/Debian_11/"
-#ARGS+=" --build-arg REPO_ROOT=${REPO_ROOT}"
-#ARGS+=" --build-arg CMAKE_BUILD_PARALLEL_LEVEL=8"
+REPO_ROOT="kerel64"
+IMAGE_NAME="satnogs-client"
+IMAGE_TAG="kerel"
+
+ARGS="  --build-arg GNURADIO_IMAGE_TAG=3.10.5.1-satnogs"
+
+ARGS+=" --build-arg GRSATNOGS_URL=https://gitlab.com/kerel-fs/gr-satnogs.git"
+ARGS+=" --build-arg GRSATNOGS_BRANCH=doppler"
+ARGS+=" --build-arg GRSATNOGS_VER=3.0.1.0~kerel20250311.43720e-1"
+
+ARGS+=" --build-arg FLOWGRAPHS_URL=https://gitlab.com/kerel-fs/satnogs-flowgraphs.git"
+ARGS+=" --build-arg FLOWGRAPHS_BRANCH=fix_doppler"
+ARGS+=" --build-arg FLOWGRAPHS_VER=1.6~kerel20250311.140979-1"
+
+ARGS+=" --build-arg CLIENT_URL=https://gitlab.com/kerel-fs/satnogs-client.git"
+ARGS+=" --build-arg CLIENT_BRANCH=kerel"
+ARGS+=" --build-arg CLIENT_VER=1.9.4~kerel20250311.3fc21ec-1"
 
 docker build \
-    -t ${REPO_ROOT}/satnogs-client:${TAG} \
+    -t ${REPO_ROOT}/${IMAGE_NAME}:${IMAGE_TAG} \
     ${ARGS} \
     . "$@"
